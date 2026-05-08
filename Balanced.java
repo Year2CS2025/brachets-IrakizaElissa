b59d6f2b4e5e4cbb0089c45ba99a2e1898dd19ec
IrakizaElissa <elisairakiza@gmail.com>
2026-05-08 10:11:05 +0200

    Add the solution of balanced string

--- Balanced.java
+++ Balanced.java
@@ -7,9 +7,20 @@ public class Balanced{
         
     }
     public static boolean isBalanced(String s){
-        //
-        
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
 }
         
     
